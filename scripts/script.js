7d9b6a5ebb3f4ccca721d8be87b0bef31a77aa9f
tfotr241290 <franco_9024@hotmail.com>
2022-10-08 02:59:22 -0500

    push partial website

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,9 @@ function toggleMenu(){
 const x = document.getElementById('hamburguerBtn');
 
 x.onclick = toggleMenu;
+
+let options = {weekday:'long',day:'numeric',month:'short',year:'numeric'};
+let currentDate = new Date();
+
+
+document.getElementById('current-date').innerHTML = currentDate.toLocaleDateString('en-US', options);
