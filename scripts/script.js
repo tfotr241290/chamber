0ad11c01ae43839c99feb2069d9b23513b9a5dc8
tfotr241290 <franco_9024@hotmail.com>
2022-10-11 13:08:44 -0500

    changed date format

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,14 @@ const x = document.getElementById('hamburguerBtn');
 
 x.onclick = toggleMenu;
 
-let options = {weekday:'long',day:'numeric',month:'short',year:'numeric'};
-let currentDate = new Date();
+//Different way to format date
+const currentDate = document.getElementById('current-date');
+const cDate = new Date();
+const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(cDate);
+currentDate.innerHTML = `${fullDate}`;
 
 
-document.getElementById('current-date').innerHTML = currentDate.toLocaleDateString('en-US', options);
+document.getElementById('current-date').innerHTML = formattedDate.toLocaleDateString('en-US')
 
 const dateOptions = {weekday:'long',day:'numeric',month:'short',year:'numeric',hour:'2-digit', minute:'2-digit'}
 const date = new Date(document.lastModified);
