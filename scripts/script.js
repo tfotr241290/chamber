1f749dcd3991540a8af09a5dc64280e49e2aa6d2
tfotr241290 <franco_9024@hotmail.com>
2022-10-13 14:34:15 -0500

    banner image first update

--- scripts/script.js
+++ scripts/script.js
@@ -7,14 +7,19 @@ const x = document.getElementById('hamburguerBtn');
 
 x.onclick = toggleMenu;
 
-//Different way to format date
+//Get the current date in customized format
 const cDate = new Date();
 const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(cDate);
 document.getElementById('current-date').innerHTML = `${fullDate}`;
 
-
-document.getElementById('current-date').innerHTML = formattedDate.toLocaleDateString('en-US')
-
+// Get the last modification date for the footer
 const dateOptions = {weekday:'long',day:'numeric',month:'short',year:'numeric',hour:'2-digit', minute:'2-digit'}
 const date = new Date(document.lastModified);
 document.getElementById("date").innerHTML = date.toLocaleDateString('en-US', dateOptions);
+
+const dayOfWeek = cDate.getDay()
+
+console.log(dayOfWeek)
+if (dayOfWeek === 4){
+    document.getElementById('banner').src = 'images/chamber-banner.jpg'
+}
