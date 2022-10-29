38fdd010ad5d57aa8c10ccddc88ecfadb8507dd9
tfotr241290 <franco_9024@hotmail.com>
2022-10-29 15:51:06 -0500

    layout completed

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,32 @@ else{
     const bannerHeader = document.getElementById('header-banner');
     bannerHeader.remove();
 }
+
+let myImages = document.querySelectorAll('img[data-src]')
+
+
+function loadImages(image){
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    }
+};
+
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    myImages.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    myImages.forEach((img) => {
+      loadImages(img);
+    });
+  }
