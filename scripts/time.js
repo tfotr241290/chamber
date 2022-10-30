d4d705c98e4151ca218e46b86533ce9c3ba68a54
tfotr241290 <franco_9024@hotmail.com>
2022-10-29 19:30:07 -0500

    assignment complete

---
+++ scripts/time.js
@@ -0,0 +1,35 @@
+const difVisit = document.querySelector(".difVisit");
+const pElement = document.querySelector(".visits p");
+
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+let lastVisit = Number(window.localStorage.getItem("lastVisit"));
+console.log(lastVisit)
+const d = new Date();
+const time = d.getTime();
+console.log(time)
+const dif = (time - lastVisit);
+console.log(dif)
+
+
+const difSeconds = Math.round(dif/1000);
+const difMinutes = Math.round(dif/minute);
+const difHours = Math.round(dif/hour);
+const difDays = Math.round(dif/day);
+let timeText = difDays + " days ";  
+difVisit.textContent = timeText;
+
+
+if (lastVisit === 0) {
+    pElement.textContent = `This is your first visit!`;
+	
+} 
+else {
+    difVisit.textContent = timeText;
+	
+}
+
+
+localStorage.setItem("lastVisit", time);
