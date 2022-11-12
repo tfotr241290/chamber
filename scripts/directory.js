db438bb97045a537d29c3c25941faaef245f72db
tfotr241290 <franco_9024@hotmail.com>
2022-11-12 02:21:44 -0600

    completed assignment

--- scripts/directory.js
+++ scripts/directory.js
@@ -1,4 +1,9 @@
-const dSource = "/chamber/json/data.json"
+const dSource = "/chamber/json/data.json";
+
+const gridButton = document.querySelector('#grid-view');
+const listButton = document.querySelector('#list-view');
+const display = document.querySelector('article');
+
 
 const requestData = async(dSource)=>{
     const response = await fetch(dSource);
@@ -7,8 +12,21 @@ const requestData = async(dSource)=>{
     if(response.ok === true){
         const data = await response.json();
         console.log(data);
-        let business = data.companies;
+        const business = data.companies;
+
+
+        listButton.addEventListener("click",()=>{
+            display.classList.add("list");
+            display.classList.remove("cards");
+            
+        });
+        gridButton.addEventListener("click",()=>{
+            display.classList.add("cards");
+            display.classList.remove("list");
+            
+        })
         business.forEach(displayCompanies);
+        
     }
     else{
         console.log('Proably the file or url you are requesting does not exist or it is wrong')
@@ -16,35 +34,40 @@ const requestData = async(dSource)=>{
 }
 
 const displayCompanies = (business)=>{
-    let card = document.createElement('section');
-    let h3 = document.createElement('h3');
-    let bImage = document.createElement('img');
-    let bWeb = document.createElement('a');
-    let bAddress = document.createElement('p');
-    let bPhone = document.createElement('p');
-    let bMembership = document.createElement('h4');
+    const card = document.createElement('section');
+    const title = document.createElement('p');
+    const bImage = document.createElement('img');
+    const bWeb = document.createElement('a');
+    const bAddress = document.createElement('p');
+    const bPhone = document.createElement('p');
+    const bMembership = document.createElement('h4');
 
     bImage.setAttribute('src', business.imgurl);
     bImage.setAttribute('alt', `Logo image of ${business.name}`);
     bImage.setAttribute('loading','lazy');
     bWeb.setAttribute('href',`${business.website}`)
-    card.setAttribute('id','card-content')
+    bWeb.setAttribute('id','b-web');
+    bAddress.setAttribute('id','b-address');
+    bPhone.setAttribute('id','b-phone');
+    title.setAttribute('id','b-title');
+    card.setAttribute('id','card-content');
+    bMembership.setAttribute('id', 'm-level');
 
-    h3.textContent = `${business.name}`;
+    title.textContent = `Company name: ${business.name}`;
     bAddress.textContent = `Address: ${business.address}`;
-    bWeb.textContent = `Website: ${business.website}`;
+    bWeb.textContent = `${business.website}`;
     bPhone.textContent = `Phone number: ${business.phone}`
     bMembership.textContent = `Membership: ${business.membershipLevel}`
 
-    card.appendChild(h3);
     card.appendChild(bImage);
+    card.appendChild(title);
     card.appendChild(bAddress);
     card.appendChild(bPhone);
     card.appendChild(bWeb);
     card.appendChild(bMembership);
 
-    document.querySelector('div.cards').appendChild(card);
-}
+    document.querySelector('.cards').appendChild(card);
+};
 
 
 requestData(dSource);
